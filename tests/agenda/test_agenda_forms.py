30c4a26937142efccb3895abdb594ce539929d6a
Bianca Cruz <bifileto@gmail.com>
2024-10-27 22:32:32 -0300

    correcao dos testes para excecao e criado teste para valor negativo

--- tests/agenda/test_agenda_forms.py
+++ tests/agenda/test_agenda_forms.py
@@ -2,6 +2,7 @@ import pytest
 from apps.agenda.models import Agenda
 from freezegun import freeze_time
 from apps.agenda.forms import AgendaForm
+from django.core.exceptions import ValidationError
 
 @pytest.mark.django_db
 def test_agenda():
@@ -11,29 +12,41 @@ def test_agenda():
 # Data fixa para todos os testes, pra o teste dar verdadeiro no caso da data fixa + 1
 @freeze_time("2024-10-27")
 @pytest.mark.django_db
-# Teste com dia permitido, maior que o dia atual
+# Teste da funcao clean_data com dia permitido, maior que o dia atual
 def test_next_day_allowed():
     agenda = AgendaForm()
     agenda.data = "2024-10-28"
-    assert AgendaForm.clean_data(agenda) == True
+    assert AgendaForm.clean_data(agenda) == agenda
 @freeze_time("2024-10-27")
 @pytest.mark.django_db
-# Teste com dia permitido, dia atual
+# Teste da funcao clean_data com dia permitido, dia atual
 def test_same_day_allowed():
     agenda = AgendaForm()
     agenda.data = "2024-10-27"
-    assert AgendaForm.clean_data(agenda) == True
+    assert AgendaForm.clean_data(agenda) == agenda
 @freeze_time("2024-10-27")
 @pytest.mark.django_db
-# Teste com dia anterior
+# Teste da funcao clean_data com dia anterior
 def test_day_not_allowed():
     agenda = AgendaForm()
     agenda.data = "2024-10-26"
-    assert AgendaForm.clean_data(agenda) == False
+    with pytest.raises(ValidationError, match='A data do treino não pode ser no passado.'):
+        AgendaForm.clean_data(agenda)
 
 @freeze_time("2024-10-27")
 @pytest.mark.django_db
+# Teste da funcao clean_data com data invalida
 def test_invalid_dates():
     agenda = AgendaForm()
     agenda.data = "invalid-date"
-    assert AgendaForm.clean_data(agenda) == False
+    with pytest.raises(ValidationError, match='A data do treino não pode ser no passado.'):
+        AgendaForm.clean_data(agenda)
+
+@pytest.mark.django_db
+#teste da funcao clean_valor com valor negativo
+def test_negative_valor():
+    agenda = AgendaForm()
+    agenda.valor = -10,9
+    with pytest.raises(ValidationError, match='O valor da aula deve ser maior que zero.'):
+        AgendaForm.clean_valor(agenda)
+
